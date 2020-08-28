b44682b5259794ce210c97749ace3bce3f96792c
JaydenM-C <jaydenshere@gmail.com>
2020-08-28 17:55:31 +1000

    Fresh datasets with internal word boundaries removed, tidied code and workflow

--- R/Ausphon_data_wrangling.R
+++ R/Ausphon_data_wrangling.R
@@ -1,30 +1,44 @@
-# Data wrangling
+#################
+# DATA WRANGLING
+#################
+
 # Jayden Macklin-Cordes
-# # # # # # # # # # # # #
+# 13 December 2019
+# Revised: 28 August 2020
+
+# This script extracts binary biphone and transition frequency data from Ausphonlex
+# NB: Includes hard-coded filepaths
+# Requires phonlex and Ausphonlex packages (not publicly released)
+
+#################
+# SET UP
+#################
 
 library(tidyverse)
+library(ape)
 library(phonlex)
 library(Ausphonlex)
 
 # Load Ausphonlex
-aus <- ausphonlex_v0.6.2
+aus <- ausphonlex_v0.6.3
 
 # Get wordlist sizes
-lex_size <- aus@entries %>% 
-  filter(str_length(xphon_form) > 0) %>% 
+lex_size <- aus@entries %>%
+  filter(str_length(xphon_form) > 0) %>%
   filter(!str_detect(tags, "duplicate|boundary_initial|boundary_final|q_mark")) %>%
-  group_by(lex_ID, entry_ID) %>% 
-  slice(1) %>% 
-  group_by(lex_ID) %>% 
-  summarise(n_entries = n()) 
+  group_by(lex_ID, entry_ID) %>%
+  slice(1) %>%
+  group_by(lex_ID) %>%
+  summarise(n_entries = n())
 
 # Get taxon labels from Map-DB-Wordlists
-# Filter to clean lex_IDs
-# Attach wordlist sizes
-# Filter out wordlists with < 250 entries
+  # Filter to clean lex_IDs
+  # Attach wordlist sizes
+  # Filter out wordlists with < 250 entries
 metadata <- read_csv("~/Desktop/Github/erichround/Ausphonlex/dbs/Map-DB-Wordlists.csv") %>%
   filter(Vocab_ID %in% as.character(get_clean_lex_IDs())) %>%
   filter(!is.na(Bowern_307_taxon_label)) %>%
+  filter(Family == "Pama-Nyungan") %>% # One non-PN language is filtered out (Lardil)
   mutate(lex_ID = as.numeric(Vocab_ID)) %>%
   select(lex_ID, tip_label = Bowern_307_taxon_label) %>%
   left_join(lex_size, by = "lex_ID") %>%
@@ -32,7 +46,7 @@ metadata <- read_csv("~/Desktop/Github/erichround/Ausphonlex/dbs/Map-DB-Wordlist
 
 write_csv(metadata, paste0("../Data/wordlist_sizes_", Sys.Date(), ".csv"))
 
-# Get wordlist data (NB INCLUDES PRIVATE DATA)
+# Get biphone forward/backward transition frequencies
 set.seed(2020)
 biphone_fwd <- as_tibble(extract_freq_dataset("biphone_fwd_clean"))
 
@@ -44,27 +58,28 @@ biphone_bkwd <- as_tibble(extract_freq_dataset("biphone_bkwd_clean"))
 #################
 
 binarize_frequencies <- function(dataset) {
-  dataset[, -c(1:2)][dataset[, -c(1:2)] > 0] = 1
+  dataset[, -1][dataset[, -1] > 0] = 1
   dataset
 }
 
 phylo_data_binary <- metadata %>% left_join(biphone_fwd, by = "lex_ID") %>%
-  binarize_frequencies %>%
-  select(-lex_ID, -n_entries)
+  select(-lex_ID, -n_entries) %>%
+  binarize_frequencies
 
-write_csv(phylo_data_binary, paste0("../data/biphone_binary_", Sys.Date(), ".csv"))
+write_csv(phylo_data_binary, paste0("../Data/biphone_binary_raw_", Sys.Date(), ".csv"))
 
 ############################
 # BIPHONE FREQUENCY DATASET
 ############################
 
-# Forward transition probabilities and backward transition probabilities for biphones:
 phylo_data_biphone_fwd <- metadata %>% left_join(biphone_fwd, by = "lex_ID") %>%
-  select(-lex_ID, -n_entries)
+  select(-lex_ID, -n_entries) %>%
+  rename_with(paste0, -one_of("tip_label"), "_fwd")
 
 phylo_data_biphone_bkwd <- metadata %>% left_join(biphone_bkwd, by = "lex_ID") %>%
-  select(-lex_ID, -n_entries)
+  select(-lex_ID, -n_entries) %>%
+  rename_with(paste0, -one_of("tip_label"), "_bkwd")
 
-write_csv(phylo_data_biphone_fwd, paste0("../data/biphone_fwd_", Sys.Date(), ".csv"))
-write_csv(phylo_data_biphone_bkwd, paste0("../data/biphone_bkwd_", Sys.Date(), ".csv"))
+phylo_data_biphone <- left_join(phylo_data_biphone_fwd, phylo_data_biphone_bkwd, by = "tip_label")
 
+write_csv(phylo_data_biphone, paste0("../Data/biphone_frequency_raw_", Sys.Date(), ".csv"))
---
+++ R/data_filtering.R
@@ -0,0 +1,74 @@
+# Data filtering
+# Jayden Macklin-Cordes
+# 20 August 2020
+# # # # # # # # # # # #
+
+# Takes raw biphone datasets extracted from Ausphonlex database and raw cognate 
+# dataset from Bouckaert et al. (2018) and performs some filtering and minimal 
+# wrangling for use with BEAST.
+
+# Input:
+#   - Data/biphone_binary_{date}.csv
+#   - Data/biphone_frequency_{date}.csv
+#   - Bouckaert_etal/Pny10-Export.tsv
+
+# Output:
+#   - Data/biphone_binary_filtered_{date}.csv
+#   - Data/biphone_frequency_filtered_{date}.csv
+#   - Data/PN_cognates_filtered_{date}.tsv
+
+library(tidyverse)
+library(ape)
+
+bin <- rev(list.files("../Data", "biphone_binary_raw", full.names = TRUE))[1] %>%
+  read_csv
+
+freq <- rev(list.files("../Data", "biphone_frequency_raw", full.names = TRUE))[1] %>%
+  read_csv
+
+cogs <- read_tsv("../Bouckaert_etal/Pny10-Export.tsv", col_names = FALSE)
+
+# BIPHONE BINARY DATA
+# Remove empty sites (all NAs), convert to nexus with "-" as NA symbol
+
+bin_filtered <- bin %>%
+  select_if(c(TRUE, sapply(2:ncol(bin), function (i) grepl("0|1", bin[,i]))))
+
+# (The sapply function selects cols containing at least one 0 or 1 value. We also 
+#  need to retain the 'tip_label' column (which obviously doesn't contain 0 or 1)
+#  so sapply gets wrapped in `c(TRUE, sapply(...))` to retain that first column)
+
+# Turn dataframe into list
+bin_list <- lapply(split(bin[,-1], 1:nrow(bin)), as.list) %>%
+  lapply(as.character) %>%
+  lapply(function (x) {x[x=="NA"] <- "-"; x})
+names(bin_list) <- bin$tip_label
+
+write.nexus.data(bin_list, paste0("../Data/biphone_binary_", Sys.Date(), ".nex"), format = "standard", interleaved = FALSE)
+
+# Change symbols field in nexus file (ape automatically lists all 9 digits instead of just 0 and 1)
+nex <- readLines(paste0("../Data/biphone_binary_", Sys.Date(), ".nex"))
+nex <- gsub("0123456789", "01", nex)
+writeLines(nex, paste0("../Data/biphone_binary_", Sys.Date(), ".nex"))
+
+# BIPHONE TRANSITION FREQUENCY DATA
+# Convert 0s to NAs, filter out invariant sites, re-write as tsv with "-" as NA symbol
+
+freq[freq == 0] <- NA
+
+freq_filtered <- freq %>%
+  # Selects columns with at least one digit (i.e. not all NAs)
+  select_if(c(TRUE, sapply(2:ncol(freq), function (i) grepl("[0-9]", freq[,i]))))
+
+# Select columns with at least two unique values
+unique_vals <- function (i) {
+  length(
+    unique(freq_filtered[,i])[!is.na(unique(freq_filtered[,i]))]
+  )
+}
+
+freq_filtered <- select_if(freq_filtered, c(TRUE, sapply(2:ncol(freq_filtered), function (i) {unique_vals(i) > 1})))
+
+write_tsv(freq_filtered, paste0("../Data/biphone_frequency_", Sys.Date(), ".tsv"))
+
+
