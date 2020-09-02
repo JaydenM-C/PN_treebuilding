7d88bca1dc1957cd8414651863868341c690fa52
JaydenM-C <jaydenshere@gmail.com>
2020-09-02 12:06:51 +1000

    Remove Arandic subgroup from biphone binary xml files

--- R/replace_data.R
+++ R/replace_data.R
@@ -15,3 +15,30 @@ dirs <- as.character(sapply(list.files("../biphone_binary"), function (i) paste(
 xml_files <- unlist(sapply(dirs, function (i) list.files(i, full.names = TRUE)))
 
 sapply(xml_files, update_xml)
+
+# Round two: Removing Arandic subgroup (pointless because it only has 1 language)
+# And updating date in output file names
+
+dirs <- as.character(sapply(list.files("../biphone_binary"), function (i) paste("../biphone_binary", i, "xml", sep = "/")))
+
+xml_files <- unlist(sapply(dirs, function (i) list.files(i, full.names = TRUE)))
+
+update_xml_date <- function (file) {
+  xml = readLines(file)
+  xml <- gsub("07-23", "08-31", xml)
+  writeLines(xml, file)
+}
+
+sapply(xml_files, update_xml_date)
+
+remove_arandic <- function (file) {
+  xml = readLines(file)
+  xml <- xml[-c(130:132)]
+  xml <- xml[-c((grep('<taxa idref="Arandic"/>', xml)[1] -1):(grep('<taxa idref="Arandic"/>', xml)[1] +2))]
+  xml <- xml[-c(grep('<tmrcaStatistic id\\="tmrca\\(Arandic\\)"', xml):(grep('<tmrcaStatistic id\\="tmrca\\(Arandic\\)"', xml) + 17))]
+  xml <- xml[-grep('<monophylyStatistic idref\\="monophyly\\(Arandic\\)"/>', xml)]
+  xml <- xml[-grep('<tmrcaStatistic idref\\="tmrca\\(Arandic\\)"/>', xml)]
+  xml <- xml[-grep('<tmrcaStatistic idref\\="age\\(Arandic\\)"/>', xml)]
+}
+
+sapply(xml_files, remove_arandic)
